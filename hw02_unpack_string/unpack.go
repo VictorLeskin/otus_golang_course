afd85e8869c9a3ef94c488f45bf7b85a4a3a8732
Victor Leskin <victorleskin16@gmail.com>
2025-10-15 23:39:43 +0300

    Revert "fix path to the package"
    This reverts commit 9cc4eeab9465b6c46fd922cd2c23a283bdfb175b.
    
    There was a extra file in the commit

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,54 +2,11 @@ package hw02unpackstring
 
 import (
 	"errors"
-	"fmt"
-	"strings"
-	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-const (
-	INIT = iota
-	SYMBOL
-)
-
-func Unpack(s string) (string, error) {
+func Unpack(_ string) (string, error) {
 	// Place your code here.
-	var state int
-	state = INIT
-	var sb strings.Builder
-	var prev rune
-
-	for _, k := range s {
-		switch state {
-
-		case INIT:
-			switch {
-			case unicode.IsDigit(k):
-				return "", ErrInvalidString
-			default:
-				prev = rune(k)
-				state = SYMBOL
-			}
-		case SYMBOL:
-			switch {
-			case unicode.IsDigit(k):
-				n := int(k)
-				for i := 0; i < n; i++ {
-					sb.WriteRune(prev)
-				}
-
-				return "", ErrInvalidString
-			default:
-				prev = rune(k)
-				state = SYMBOL
-			}
-
-		}
-
-		fmt.Printf("%d %d = %c = %s\n", state, prev, k, sb.String())
-
-	}
-	return sb.String(), nil
+	return "", nil
 }
