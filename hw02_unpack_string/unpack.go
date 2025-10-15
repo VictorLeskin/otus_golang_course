9cc4eeab9465b6c46fd922cd2c23a283bdfb175b
Victor Leskin <victorleskin16@gmail.com>
2025-10-15 23:27:28 +0300

    fix path to the package

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,54 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"fmt"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
+const (
+	INIT = iota
+	SYMBOL
+)
+
+func Unpack(s string) (string, error) {
 	// Place your code here.
-	return "", nil
+	var state int
+	state = INIT
+	var sb strings.Builder
+	var prev rune
+
+	for _, k := range s {
+		switch state {
+
+		case INIT:
+			switch {
+			case unicode.IsDigit(k):
+				return "", ErrInvalidString
+			default:
+				prev = rune(k)
+				state = SYMBOL
+			}
+		case SYMBOL:
+			switch {
+			case unicode.IsDigit(k):
+				n := int(k)
+				for i := 0; i < n; i++ {
+					sb.WriteRune(prev)
+				}
+
+				return "", ErrInvalidString
+			default:
+				prev = rune(k)
+				state = SYMBOL
+			}
+
+		}
+
+		fmt.Printf("%d %d = %c = %s\n", state, prev, k, sb.String())
+
+	}
+	return sb.String(), nil
 }
