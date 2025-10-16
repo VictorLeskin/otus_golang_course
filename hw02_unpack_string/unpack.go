44d846b37833b44e0cc3ec8f25217a04e44b2fea
Victor Leskin <victorleskin16@gmail.com>
2025-10-16 23:56:17 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,66 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
+	"unicode"
+	"unicode/utf8"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
+const (
+	INIT = iota
+	SYMBOL
+	BACKSLASH
+)
+
+func Unpack(s string) (string, error) {
 	// Place your code here.
-	return "", nil
+	state := INIT
+	prev := utf8.RuneError
+	var sb strings.Builder
+
+	for _, k := range s {
+		switch state {
+		case INIT:
+			switch {
+			case unicode.IsDigit(k):
+				return "", ErrInvalidString
+			case k == '\\':
+				state = BACKSLASH
+			default:
+				prev = k
+				state = SYMBOL
+			}
+		case SYMBOL:
+			switch {
+			case unicode.IsDigit(k):
+				n := int(k - '0')
+				for i := 0; i < n; i++ {
+					sb.WriteRune(prev)
+				}
+				prev = utf8.RuneError
+				state = INIT
+			case k == '\\':
+				sb.WriteRune(prev)
+				state = BACKSLASH
+			default:
+				sb.WriteRune(prev)
+				prev = k
+			}
+		case BACKSLASH:
+			switch {
+			case unicode.IsDigit(k) || k == '\\':
+				prev = k
+				state = SYMBOL
+			default:
+				return "", nil // only \\ or \0-9 are allowed
+			}
+		}
+		// fmt.Printf("%d %d = %c = %s\n", state, prev, k, sb.String())
+	}
+	if prev != utf8.RuneError {
+		sb.WriteRune(prev)
+	}
+	return sb.String(), nil
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -2,10 +2,22 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
+
+func TestDebugPackage(_ *testing.T) {
+	result, err := Unpack(`qwe\4\5`)
+	if err != nil {
+		fmt.Print("Ups..")
+	}
+
+	if result != `qwe45` {
+		fmt.Print("Ups..#2")
+	}
+}
 
 func TestUnpack(t *testing.T) {
 	tests := []struct {
@@ -19,14 +31,13 @@ func TestUnpack(t *testing.T) {
 		{input: "🙃0", expected: ""},
 		{input: "aaф0b", expected: "aab"},
 		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
 	}
 
 	for _, tc := range tests {
-		tc := tc
 		t.Run(tc.input, func(t *testing.T) {
 			result, err := Unpack(tc.input)
 			require.NoError(t, err)
@@ -38,7 +49,6 @@ func TestUnpack(t *testing.T) {
 func TestUnpackInvalidString(t *testing.T) {
 	invalidStrings := []string{"3abc", "45", "aaa10b"}
 	for _, tc := range invalidStrings {
-		tc := tc
 		t.Run(tc, func(t *testing.T) {
 			_, err := Unpack(tc)
 			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
