5839a3582af07c5c8b9b708ed2034ed797942e1a
Victor Leskin <victorleskin16@gmail.com>
2025-10-20 08:47:46 +0300

    Initial commit of homework3

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,49 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	//	"fmt"
+	"sort"
+	"strings"
+)
+
+type wordStat struct {
+	word  string
+	count int
+}
+
+var TopSize int = 10
+
+func Top10(text string) []string {
+
+	var ret []string
+	words := strings.Fields(text)
+	wordCount := make(map[string]int)
+
+	for _, r := range words {
+		wordCount[r]++
+	}
+
+	if len(wordCount) < TopSize {
+		return ret
+	}
+
+	uniqueWordsStat := make([]wordStat, 0, len(wordCount))
+	for word, cnt := range wordCount {
+		uniqueWordsStat = append(uniqueWordsStat, wordStat{word: word, count: cnt})
+	}
+
+	sort.Slice(uniqueWordsStat, func(i, j int) bool {
+		if uniqueWordsStat[i].count == uniqueWordsStat[j].count {
+			return uniqueWordsStat[i].word < uniqueWordsStat[j].word
+		}
+		return uniqueWordsStat[i].count > uniqueWordsStat[j].count
+	})
+
+	for i := 0; i < TopSize; i++ {
+		ret = append(ret, uniqueWordsStat[i].word)
+	}
+	// fmt.Println(uniqueWordsStat)
+
+	// return nil
+	return ret
 }
