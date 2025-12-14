6d9bca45b58a4e2cd6f042825251eda0b918344e
Victor Leskin <victorleskin16@gmail.com>
2025-12-14 15:20:31 +0300

    Add parsing command line arguments

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"errors"
+	"flag"
+	"fmt"
 )
 
 var (
@@ -9,7 +11,40 @@ var (
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
+type CommanLineParameter struct {
+	input, output string
+	offset, limit int64
+}
+
 func Copy(fromPath, toPath string, offset, limit int64) error {
 	// Place your code here.
 	return nil
+}
+
+func Usage() {
+	fmt.Println("Copy a file or a part of it.")
+	flag.PrintDefaults()
+}
+
+func SetupCommadLineParameters() {
+	flag.Usage = Usage
+}
+
+func ParseCommadLine() (ret CommanLineParameter, err error) {
+
+	// Copy a part of a file according to the operands.
+	flag.StringVar(&ret.input, "from", "", "file to read from")
+	flag.StringVar(&ret.output, "to", "", "file to copy")
+	flag.Int64Var(&ret.offset, "offset", 0, "skip offset bytes at start of output")
+	flag.Int64Var(&ret.limit, "limit", 0, "copy only 'limit' bytes")
+
+	flag.Parse() // проанализировать аргументы
+
+	if ret.input == "" {
+		return ret, errors.New("there is not name of the file to read from")
+	}
+	if ret.output == "" {
+		return ret, errors.New("there is not name of the file to copy")
+	}
+	return ret, nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,91 @@
 package main
 
-import "testing"
+import (
+	"flag"
+	"os"
+
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestCopy(t *testing.T) {
 	// Place your code here.
+}
+
+// Вспомогательная функция для сброса состояния флагов
+func resetFlag() {
+	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+}
+
+func TestParseCommadLine(t *testing.T) {
+	type sExpectedData struct {
+		input, output string
+		offset, limit int64
+	}
+
+	tests := []struct {
+		name         string
+		args         []string
+		expectError  string
+		expectedData sExpectedData
+	}{
+		{
+			name:         "valid parameters",
+			args:         []string{"program", "-from", "in.txt", "-to", "out.txt", "-offset", "5", "-limit", "10"},
+			expectError:  "",
+			expectedData: sExpectedData{"in.txt", "out.txt", 5, 10},
+		},
+		{
+			name:        "missing input",
+			args:        []string{"program", "-to", "out.txt"},
+			expectError: "there is not name of the file to read from",
+		},
+		{
+			name:        "missing output",
+			args:        []string{"program", "-from", "in.txt"},
+			expectError: "there is not name of the file to copy",
+		},
+		{
+			name:         "only required parameters",
+			args:         []string{"program", "-from", "in.txt", "-to", "out.txt"},
+			expectError:  "",
+			expectedData: sExpectedData{"in.txt", "out.txt", 0, 0},
+		},
+		{
+			name:         "garbage in numbers",
+			args:         []string{"program", "-from", "in.txt", "-to", "out.txt", "ABCD", "EFG"},
+			expectError:  "",
+			expectedData: sExpectedData{"in.txt", "out.txt", 0, 0},
+		},
+		{
+			name:        "missed mandatory parameters",
+			args:        []string{"program"},
+			expectError: "there is not name of the file to read from",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			resetFlag()
+
+			oldArgs := os.Args
+			defer func() { os.Args = oldArgs }()
+
+			os.Args = tt.args
+
+			result, err := ParseCommadLine()
+
+			if tt.expectError == "" {
+				assert.Nil(t, err)
+				assert.Equal(t, tt.expectedData.input, result.input)
+				assert.Equal(t, tt.expectedData.output, result.output)
+				assert.Equal(t, tt.expectedData.offset, result.offset)
+				assert.Equal(t, tt.expectedData.limit, result.limit)
+			} else {
+				assert.NotNil(t, err)
+				assert.Equal(t, tt.expectError, err.Error())
+			}
+		})
+	}
 }
