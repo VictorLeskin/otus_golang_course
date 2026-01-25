2ad27090f6328e4aa47668441427bdbfb178d3db
Victor Leskin <victorleskin16@gmail.com>
2026-01-25 08:00:11 +0300

    Add functions for LenValidator

--- hw09_struct_validator/field_validation_test.go
+++ hw09_struct_validator/field_validation_test.go
@@ -1,6 +1,7 @@
 package hw09structvalidator
 
 import (
+	"fmt"
 	"reflect"
 	"strings"
 	"testing"
@@ -9,33 +10,6 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func Test_LenValidator_ctor(t *testing.T) {
-	t0 := &LenValidator{limit: 8}
-
-	var t1 RuleValidator
-	t1 = t0
-
-	_ = t1
-
-}
-
-func Test_MinValidator_createRuleMin(t *testing.T) {
-	{
-		res, err := createRuleMin("44")
-		require.NoError(t, err)
-		require.NotNil(t, res)
-
-		res1 := res.(*MinValidator)
-		assert.Equal(t, 44, res1.limit)
-	}
-
-	{
-		_, err := createRuleMin("A")
-		expectedText := "An invalid value in the rule 'min'"
-		assert.Equal(t, true, strings.Contains(err.Error(), expectedText))
-	}
-}
-
 type LimitValidatableNumericalTypes interface {
 	~int | ~int8 | ~int16 | ~int32 | ~int64 |
 		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64 |
@@ -43,16 +17,19 @@ type LimitValidatableNumericalTypes interface {
 }
 
 type TUser[T LimitValidatableNumericalTypes] struct {
-	Age    T
-	Scores []T
+	Age         T
+	Scores      []T
+	Unsupported interface{}
+	Name        string
+	Books       []string
 }
 
-func Test_MinValidator_ValidateValue_Int(t *testing.T) {
+func Test_RuleValidator_ValidateValue(t *testing.T) {
 	t0, _ := createRuleMin("42")
 
 	type User = TUser[int]
 
-	// successful validated of single int
+	// successful validating of single int
 	{
 		user := User{Age: 43}
 		rt := reflect.TypeOf(user)
@@ -64,7 +41,7 @@ func Test_MinValidator_ValidateValue_Int(t *testing.T) {
 			rt: rt,
 		}
 
-		err1 := t0.ValidateValue(validator, field, rv.FieldByName("Age"))
+		err1 := ValidateValue(t0, validator, field, rv.FieldByName("Age"))
 		assert.NoError(t, err1)
 		assert.Equal(t, 0, len(validator.vErrors))
 	}
@@ -81,14 +58,14 @@ func Test_MinValidator_ValidateValue_Int(t *testing.T) {
 			rt: rt,
 		}
 
-		err1 := t0.ValidateValue(validator, field, rv.FieldByName("Age"))
+		err1 := ValidateValue(t0, validator, field, rv.FieldByName("Age"))
 		assert.NoError(t, err1)
 		assert.Equal(t, 1, len(validator.vErrors))
 		expectedText := "Validating error of member 'Age' of struct 'TUser[int]' by rule 'min'"
 		assert.Equal(t, expectedText, validator.vErrors[0].Error())
 	}
 
-	// successful validated of slice int
+	// successful validating of slice int
 	{
 		user := User{Scores: []int{43, 45}}
 		rt := reflect.TypeOf(user)
@@ -100,7 +77,7 @@ func Test_MinValidator_ValidateValue_Int(t *testing.T) {
 			rt: rt,
 		}
 
-		err1 := t0.ValidateValue(validator, field, rv.FieldByName("Scores"))
+		err1 := ValidateValue(t0, validator, field, rv.FieldByName("Scores"))
 		assert.NoError(t, err1)
 		assert.Equal(t, 0, len(validator.vErrors))
 	}
@@ -117,7 +94,7 @@ func Test_MinValidator_ValidateValue_Int(t *testing.T) {
 			rt: rt,
 		}
 
-		err1 := t0.ValidateValue(validator, field, rv.FieldByName("Scores"))
+		err1 := ValidateValue(t0, validator, field, rv.FieldByName("Scores"))
 		assert.NoError(t, err1)
 		assert.Equal(t, 2, len(validator.vErrors))
 		expectedText0 := "Validating error of member 'Scores[0]' of struct 'TUser[int]' by rule 'min'"
@@ -126,99 +103,191 @@ func Test_MinValidator_ValidateValue_Int(t *testing.T) {
 		assert.Equal(t, expectedText1, validator.vErrors[1].Error())
 	}
 
-}
-
-func TTest_MinValidator_ValidateValue[T LimitValidatableNumericalTypes](t *testing.T) {
-	t0, _ := createRuleMin("42")
-
-	type User = TUser[T]
-
-	// successful validated of single int
+	// wrong type
 	{
 		user := User{Age: 43}
 		rt := reflect.TypeOf(user)
 		rv := reflect.ValueOf(user)
-		field, _ := rt.FieldByName("Age")
+		field, _ := rt.FieldByName("Unsupported")
 
 		validator := &CValidator{ //  CValidator with only neccessary fields
 			rv: rv,
 			rt: rt,
 		}
 
-		err1 := t0.ValidateValue(validator, field, rv.FieldByName("Age"))
-		assert.NoError(t, err1)
-		assert.Equal(t, 0, len(validator.vErrors))
-	}
-
-	// failed validated of single int
-	{
-		user := User{Age: 41}
-		rt := reflect.TypeOf(user)
-		rv := reflect.ValueOf(user)
-		field, _ := rt.FieldByName("Age")
-
-		validator := &CValidator{ //  CValidator with only neccessary fields
-			rv: rv,
-			rt: rt,
-		}
-
-		err1 := t0.ValidateValue(validator, field, rv.FieldByName("Age"))
-		assert.NoError(t, err1)
-		assert.Equal(t, 1, len(validator.vErrors))
-		expectedText := "Validating error of member 'Age' of struct 'TUser[int]' by rule 'min'"
-		assert.Equal(t, expectedText, validator.vErrors[0].Error())
-	}
-
-	// successful validated of slice int
-	{
-		user := User{Scores: []T{43, 45}}
-		rt := reflect.TypeOf(user)
-		rv := reflect.ValueOf(user)
-		field, _ := rt.FieldByName("Scores")
-
-		validator := &CValidator{ //  CValidator with only neccessary fields
-			rv: rv,
-			rt: rt,
-		}
-
-		err1 := t0.ValidateValue(validator, field, rv.FieldByName("Scores"))
-		assert.NoError(t, err1)
-		assert.Equal(t, 0, len(validator.vErrors))
-	}
-
-	// failed validated of slice int
-	{
-		user := User{Scores: []T{41, 40}}
-		rt := reflect.TypeOf(user)
-		rv := reflect.ValueOf(user)
-		field, _ := rt.FieldByName("Scores")
-
-		validator := &CValidator{ //  CValidator with only neccessary fields
-			rv: rv,
-			rt: rt,
-		}
-
-		err1 := t0.ValidateValue(validator, field, rv.FieldByName("Scores"))
-		assert.NoError(t, err1)
-		assert.Equal(t, 2, len(validator.vErrors))
-		expectedText0 := "Validating error of member 'Scores[0]' of struct 'TUser[int]' by rule 'min'"
-		expectedText1 := "Validating error of member 'Scores[1]' of struct 'TUser[int]' by rule 'min'"
-		assert.Equal(t, expectedText0, validator.vErrors[0].Error())
-		assert.Equal(t, expectedText1, validator.vErrors[1].Error())
+		err1 := ValidateValue(t0, validator, field, rv.FieldByName("Unsupported"))
+		assert.Error(t, err1)
+		expectedText := "Non unsupported type 'interface' by rule 'min'"
+		assert.Equal(t, expectedText, err1.Error())
 	}
 }
 
-func Test_MinValidator_ValidateValue_AllNumericTypes(t *testing.T) {
-	TTest_MinValidator_ValidateValue[int](t)
-	TTest_MinValidator_ValidateValue[int8](t)
-	TTest_MinValidator_ValidateValue[int16](t)
-	TTest_MinValidator_ValidateValue[int32](t)
-	TTest_MinValidator_ValidateValue[int64](t)
-	TTest_MinValidator_ValidateValue[uint](t)
-	TTest_MinValidator_ValidateValue[uint8](t)
-	TTest_MinValidator_ValidateValue[uint16](t)
-	TTest_MinValidator_ValidateValue[uint32](t)
-	TTest_MinValidator_ValidateValue[uint64](t)
-	TTest_MinValidator_ValidateValue[float32](t)
-	TTest_MinValidator_ValidateValue[float64](t)
+/************ LenValidator ************/
+func Test_createRuleLen(t *testing.T) {
+	{
+		res, err := createRuleLen("44")
+		require.NoError(t, err)
+		require.NotNil(t, res)
+
+		res1 := res.(*LenValidator)
+		assert.Equal(t, 44, res1.limit)
+	}
+
+	{
+		_, err := createRuleLen("A")
+		expectedText := "An invalid value in the rule 'len'"
+		assert.Equal(t, true, strings.Contains(err.Error(), expectedText))
+	}
+}
+
+func Test_LenValidator_ValidateValue0(t *testing.T) {
+	t0, _ := createRuleLen("6")
+	type User = TUser[int]
+
+	// successful validating
+	{
+		user := User{Name: "012345"}
+
+		rt := reflect.TypeOf(user)
+		rv := reflect.ValueOf(user.Name)
+
+		validator := &CValidator{ //  CValidator with only neccessary fields
+			rv: rv,
+			rt: rt,
+		}
+
+		err1 := t0.ValidateValue0(validator, "Name", rv.Type().Kind(), rv, -1)
+		assert.NoError(t, err1)
+		assert.Equal(t, 0, len(validator.vErrors))
+	}
+
+	// unsuccessful validating
+	{
+		user := User{Name: "very long name"}
+
+		rt := reflect.TypeOf(user)
+		rv := reflect.ValueOf(user.Name)
+
+		validator := &CValidator{ //  CValidator with only neccessary fields
+			rv: rv,
+			rt: rt,
+		}
+
+		err1 := t0.ValidateValue0(validator, "Name", rv.Type().Kind(), rv, -1)
+		assert.NoError(t, err1)
+		assert.Equal(t, 1, len(validator.vErrors))
+		expectedText := fmt.Sprintf("Validating error of member 'Name' of struct '%s' by rule 'len'", rt.Name())
+		assert.Equal(t, expectedText, validator.vErrors[0].Error())
+	}
+
+	// unsupported type
+	{
+		user := User{Name: "012345"}
+
+		rt := reflect.TypeOf(user)
+		rv := reflect.ValueOf(user)
+
+		validator := &CValidator{ //  CValidator with only neccessary fields
+			rv: rv,
+			rt: rt,
+		}
+
+		err1 := t0.ValidateValue0(validator, "Name", rv.Type().Kind(), rv, -1)
+		assert.Error(t, err1)
+		expectedText := "Non unsupported type 'struct' by rule 'len'"
+		assert.Equal(t, expectedText, err1.Error())
+	}
+}
+
+/************ MinValidator ************/
+func Test_createRuleMin(t *testing.T) {
+	{
+		res, err := createRuleMin("44")
+		require.NoError(t, err)
+		require.NotNil(t, res)
+
+		res1 := res.(*MinValidator)
+		assert.Equal(t, int64(44), res1.limit)
+	}
+
+	{
+		_, err := createRuleMin("A")
+		expectedText := "An invalid value in the rule 'min'"
+		assert.Equal(t, true, strings.Contains(err.Error(), expectedText))
+	}
+}
+
+func TTest_MinValidator_ValidateValue0[T LimitValidatableNumericalTypes](t *testing.T) {
+
+	t0, _ := createRuleMin("42")
+	type User = TUser[T]
+
+	// successful validating of single int
+	{
+		user := User{Age: 43}
+
+		rt := reflect.TypeOf(user)
+		rv := reflect.ValueOf(user.Age)
+
+		validator := &CValidator{ //  CValidator with only neccessary fields
+			rv: rv,
+			rt: rt,
+		}
+
+		err1 := t0.ValidateValue0(validator, "Age", rv.Type().Kind(), rv, -1)
+		assert.NoError(t, err1)
+		assert.Equal(t, 0, len(validator.vErrors))
+	}
+
+	// unsuccessful validating of single int
+	{
+		user := User{Age: 41}
+
+		rt := reflect.TypeOf(user)
+		rv := reflect.ValueOf(user.Age)
+
+		validator := &CValidator{ //  CValidator with only neccessary fields
+			rv: rv,
+			rt: rt,
+		}
+
+		err1 := t0.ValidateValue0(validator, "Age", rv.Type().Kind(), rv, -1)
+		assert.NoError(t, err1)
+		assert.Equal(t, 1, len(validator.vErrors))
+		expectedText := fmt.Sprintf("Validating error of member 'Age' of struct '%s' by rule 'min'", rt.Name())
+		assert.Equal(t, expectedText, validator.vErrors[0].Error())
+	}
+
+	// unsupported type
+	{
+		user := User{Age: 41}
+
+		rt := reflect.TypeOf(user)
+		rv := reflect.ValueOf(user)
+
+		validator := &CValidator{ //  CValidator with only neccessary fields
+			rv: rv,
+			rt: rt,
+		}
+
+		err1 := t0.ValidateValue0(validator, "Age", rv.Type().Kind(), rv, -1)
+		assert.Error(t, err1)
+		expectedText := "Non unsupported type 'struct' by rule 'min'"
+		assert.Equal(t, expectedText, err1.Error())
+	}
+}
+
+func Test_MinValidator_ValidateValue0_Int(t *testing.T) {
+	TTest_MinValidator_ValidateValue0[int](t)
+	TTest_MinValidator_ValidateValue0[int8](t)
+	TTest_MinValidator_ValidateValue0[int16](t)
+	TTest_MinValidator_ValidateValue0[int32](t)
+	TTest_MinValidator_ValidateValue0[int64](t)
+	TTest_MinValidator_ValidateValue0[uint](t)
+	TTest_MinValidator_ValidateValue0[uint8](t)
+	TTest_MinValidator_ValidateValue0[uint16](t)
+	TTest_MinValidator_ValidateValue0[uint32](t)
+	TTest_MinValidator_ValidateValue0[uint64](t)
+	TTest_MinValidator_ValidateValue0[float32](t)
+	TTest_MinValidator_ValidateValue0[float64](t)
 }
