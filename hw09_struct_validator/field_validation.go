4084e3f49e2ebda3e278a2f2a9b22b8261e3fb33
Victor Leskin <victorleskin16@gmail.com>
2026-01-25 09:30:39 +0300

    Add functions for all validators

--- hw09_struct_validator/field_validation.go
+++ hw09_struct_validator/field_validation.go
@@ -3,6 +3,7 @@ package hw09structvalidator
 import (
 	"fmt"
 	"reflect"
+	"regexp"
 	"strconv"
 	"strings"
 )
@@ -48,12 +49,19 @@ type MaxValidator struct {
 
 type RegexpValidator struct {
 	RuleValidator
-	regex string
+	s  string        // initial string
+	re regexp.Regexp // trasformed to regular expression
 }
 
 type InValidator struct {
 	RuleValidator
 	enabled []string
+
+	enabledInt    []int
+	errEnabledInt error
+
+	enabledFlt    []float64
+	errEnabledFlt error
 }
 
 func (v *LenValidator) ValidateValue0(parent *CValidator, name string, kind reflect.Kind, rv reflect.Value, index int) error {
@@ -91,37 +99,108 @@ func (v *MinValidator) ValidateValue0(parent *CValidator, name string, kind refl
 	return nil
 }
 
-/*
-func (v *MaxValidator) ValidateValue(parent *CValidator, tp reflect.StructField, rv reflect.Value) error {
-	// get type and value:
-	rt := tp.Type
+func (v *MaxValidator) ValidateValue0(parent *CValidator, name string, kind reflect.Kind, rv reflect.Value, index int) error {
+	switch kind {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64: //  int
+		if rv.Int() > v.limit {
+			parent.appendValidatingError(v.Name(), name, index)
+		}
 
-	fmt.Printf("Max validator\n")
-	fmt.Printf("Type: %v\n", rt)
-	fmt.Printf("Value: %v\n", rv)
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64: // uint
+		if rv.Uint() > uint64(v.limit) {
+			parent.appendValidatingError(v.Name(), name, index)
+		}
+
+	case reflect.Float32, reflect.Float64: // floatt
+		if rv.Float() > float64(v.limit) {
+			parent.appendValidatingError(v.Name(), name, index)
+		}
+	default:
+		return fmt.Errorf("Non unsupported type '%s' by rule '%s'", kind.String(), v.Name())
+	}
 	return nil
 }
 
-func (v *RegexpValidator) ValidateValue(parent *CValidator, tp reflect.StructField, rv reflect.Value) error {
-	// get type and value:
-	rt := tp.Type
+func (v *RegexpValidator) ValidateValue0(parent *CValidator, name string, kind reflect.Kind, rv reflect.Value, index int) error {
+	switch kind {
+	case reflect.String:
+		if !v.re.MatchString(rv.String()) {
+			parent.appendValidatingError(v.Name(), name, index)
+		}
 
-	fmt.Printf("Regexp validator\n")
-	fmt.Printf("Type: %v\n", rt)
-	fmt.Printf("Value: %v\n", rv)
+	default:
+		return fmt.Errorf("Non unsupported type '%s' by rule '%s'", kind.String(), v.Name())
+	}
 	return nil
 }
 
-func (v *InValidator) ValidateValue(parent *CValidator, tp reflect.StructField, rv reflect.Value) error {
-	// get type and value:
-	rt := tp.Type
+func (v *InValidator) toInts() (ret []int, err error) {
+	for _, value := range v.enabled {
+		i, err := strconv.Atoi(value)
+		if err != nil {
+			return nil, fmt.Errorf("invalid integer value %s for in value in rule 'in' %v", value, err)
+		}
+		ret = append(ret, i)
+	}
 
-	fmt.Printf("In validator\n")
-	fmt.Printf("Type: %v\n", rt)
-	fmt.Printf("Value: %v\n", rv)
+	return ret, err
+}
+
+func (v *InValidator) toFloats() (ret []float64, err error) {
+	for _, value := range v.enabled {
+		f, err := strconv.ParseFloat(value, 64)
+		if err != nil {
+			return nil, fmt.Errorf("invalid integer value %s for in value in rule 'in' %v", value, err)
+		}
+		ret = append(ret, f)
+	}
+
+	return ret, err
+}
+
+func (v *InValidator) ValidateValue0(parent *CValidator, name string, kind reflect.Kind, rv reflect.Value, index int) error {
+	switch kind {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64: //  int
+		if v.errEnabledInt != nil {
+			return v.errEnabledInt
+		}
+		for _, e := range v.enabledInt {
+			if rv.Int() == int64(e) {
+				return nil
+			}
+		}
+
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64: // uint
+		if v.errEnabledInt != nil {
+			return v.errEnabledInt
+		}
+		for _, e := range v.enabledInt {
+			if rv.Uint() == uint64(e) {
+				return nil
+			}
+		}
+
+	case reflect.Float32, reflect.Float64: // floatt
+		if v.errEnabledFlt != nil {
+			return v.errEnabledFlt
+		}
+		for _, e := range v.enabledFlt {
+			if rv.Float() == e {
+				return nil
+			}
+		}
+	case reflect.String:
+		for _, e := range v.enabled {
+			if rv.String() == e {
+				return nil
+			}
+		}
+	default:
+		return fmt.Errorf("Non unsupported type '%s' by rule '%s'", kind.String(), v.Name())
+	}
+	parent.appendValidatingError(v.Name(), name, index)
 	return nil
 }
-*/
 
 func CreateRule(name string, value string) (RuleValidator, error) {
 	switch name {
@@ -174,14 +253,22 @@ func createRuleMax(value string) (RuleValidator, error) {
 
 func createRuleRegexp(value string) (RuleValidator, error) {
 	// regex.Compile()
+	re, err := regexp.Compile(value) // возвращает ошибку
+	if err != nil {
+		return nil, fmt.Errorf("An invalid value in the rule 'regexp': %v", err)
+	}
 
 	return &RegexpValidator{
-		regex: value}, nil
+		s: value, re: *re}, nil
 }
 
 func createRuleIn(value string) (RuleValidator, error) {
-	return &InValidator{
-		enabled: strings.Split(value, ",")}, nil
+	ret := &InValidator{
+		enabled: strings.Split(value, ",")}
+	ret.enabledInt, ret.errEnabledInt = ret.toInts()
+	ret.enabledFlt, ret.errEnabledFlt = ret.toFloats()
+
+	return ret, nil
 }
 
 func (v *LenValidator) Name() string    { return "len" }
