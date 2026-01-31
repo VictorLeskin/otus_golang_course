17e32b8e21423ae802337e87c801ce31ad975e1b
Victor Leskin <victorleskin16@gmail.com>
2026-01-31 17:03:16 +0300

    Make some refactoring to reduce CPU and memory consumptions

--- hw10_program_optimization/my_stats_test.go
+++ hw10_program_optimization/my_stats_test.go
@@ -17,6 +17,14 @@ var data1 string = `{"Id":1,"Name":"Howard Mendoza","Username":"0Oliver","Email"
 {"Id":4,"Name":"Gregory Reid","Username":"tButler","Email":"5Moore@Teklist.net","Phone":"520-04-16","Password":"r639qLNu","Address":"Sunfield Park 20"}
 {"Id":5,"Name":"Janice Rose","Username":"KeithHart","Email":"nulla@Linktype.com","Phone":"146-91-01","Password":"acSBF5","Address":"Russell Trail 61"}`
 
+var data2 []string = []string{
+	`{"Id":1,"Name":"Howard Mendoza","Username":"0Oliver","Email":"aliquid_qui_ea@Browsedrive.gov","Phone":"6-866-899-36-79","Password":"InAQJvsq","Address":"Blackbird Place 25"}`,
+	`{"Id":2,"Name":"Jesse Vasquez","Username":"qRichardson","Email":"mLynch@broWsecat.com","Phone":"9-373-949-64-00","Password":"SiZLeNSGn","Address":"Fulton Hill 80"}`,
+	`{"Id":3,"Name":"Clarence Olson","Username":"RachelAdams","Email":"RoseSmith@Browsecat.com","Phone":"988-48-97","Password":"71kuz3gA5w","Address":"Monterey Park 39"}`,
+	`{"Id":4,"Name":"Gregory Reid","Username":"tButler","Email":"5Moore@Teklist.net","Phone":"520-04-16","Password":"r639qLNu","Address":"Sunfield Park 20"}`,
+	`{"Id":5,"Name":"Janice Rose","Username":"KeithHart","Email":"nulla@Linktype.com","Phone":"146-91-01","Password":"acSBF5","Address":"Russell Trail 61"}`,
+}
+
 type MockReader struct {
 	err    error
 	readed int
@@ -44,30 +52,30 @@ func Test_getUsers(t *testing.T) {
 		readed: 0,
 		buffer: []byte(data1),
 	}
-
-	users, err := original_getUsers(reader)
+	var u users
+	err := getUsers(reader, &u)
 
 	assert.Nil(t, err)
 
-	require.Less(t, 5, len(users))
-	assert.Equal(t, users[0].ID, 1)
-	assert.Equal(t, users[0].Name, "Howard Mendoza")
-	assert.Equal(t, users[0].Username, "0Oliver")
-	assert.Equal(t, users[0].Email, "aliquid_qui_ea@Browsedrive.gov")
-	assert.Equal(t, users[0].Phone, "6-866-899-36-79")
-	assert.Equal(t, users[0].Password, "InAQJvsq")
-	assert.Equal(t, users[0].Address, "Blackbird Place 25")
+	require.Less(t, 5, len(u))
+	assert.Equal(t, u[0].ID, 1)
+	assert.Equal(t, u[0].Name, "Howard Mendoza")
+	assert.Equal(t, u[0].Username, "0Oliver")
+	assert.Equal(t, u[0].Email, "aliquid_qui_ea@Browsedrive.gov")
+	assert.Equal(t, u[0].Phone, "6-866-899-36-79")
+	assert.Equal(t, u[0].Password, "InAQJvsq")
+	assert.Equal(t, u[0].Address, "Blackbird Place 25")
 
-	assert.Equal(t, users[4].ID, 5)
-	assert.Equal(t, users[4].Name, "Janice Rose")
-	assert.Equal(t, users[4].Username, "KeithHart")
-	assert.Equal(t, users[4].Email, "nulla@Linktype.com")
-	assert.Equal(t, users[4].Phone, "146-91-01")
-	assert.Equal(t, users[4].Password, "acSBF5")
-	assert.Equal(t, users[4].Address, "Russell Trail 61")
+	assert.Equal(t, u[4].ID, 5)
+	assert.Equal(t, u[4].Name, "Janice Rose")
+	assert.Equal(t, u[4].Username, "KeithHart")
+	assert.Equal(t, u[4].Email, "nulla@Linktype.com")
+	assert.Equal(t, u[4].Phone, "146-91-01")
+	assert.Equal(t, u[4].Password, "acSBF5")
+	assert.Equal(t, u[4].Address, "Russell Trail 61")
 
-	assert.Equal(t, users[5].ID, 0)
-	assert.Equal(t, users[5].Email, "")
+	assert.Equal(t, u[5].ID, 0)
+	assert.Equal(t, u[5].Email, "")
 }
 
 func Test_Match(t *testing.T) {
@@ -167,4 +175,60 @@ func Benchmark_updateDomainStatS(b *testing.B) {
 		}()
 	}
 
+}
+
+func Test_Unmarshal(t *testing.T) {
+	data := data2[0]
+	var user User
+	err := Unmarshal(data, &user)
+
+	assert.Nil(t, err)
+	assert.Equal(t, user.ID, 1)
+	assert.Equal(t, user.Name, "Howard Mendoza")
+	assert.Equal(t, user.Username, "0Oliver")
+	assert.Equal(t, user.Email, "aliquid_qui_ea@Browsedrive.gov")
+	assert.Equal(t, user.Phone, "6-866-899-36-79")
+	assert.Equal(t, user.Password, "InAQJvsq")
+	assert.Equal(t, user.Address, "Blackbird Place 25")
+}
+
+func Test_UnmarshalS(t *testing.T) {
+	data := data2[0]
+	var user User
+	err := UnmarshalS(data, &user)
+
+	assert.Nil(t, err)
+	assert.Equal(t, user.ID, 1)
+	assert.Equal(t, user.Name, "Howard Mendoza")
+	assert.Equal(t, user.Username, "0Oliver")
+	assert.Equal(t, user.Email, "aliquid_qui_ea@Browsedrive.gov")
+	assert.Equal(t, user.Phone, "6-866-899-36-79")
+	assert.Equal(t, user.Password, "InAQJvsq")
+	assert.Equal(t, user.Address, "Blackbird Place 25")
+}
+
+func Benchmark_Unmarshal(b *testing.B) {
+	var user User
+	for i := 0; i < b.N; i++ {
+		func() {
+			Unmarshal(data2[0], &user)
+			Unmarshal(data2[1], &user)
+			Unmarshal(data2[2], &user)
+			Unmarshal(data2[3], &user)
+			Unmarshal(data2[4], &user)
+		}()
+	}
+}
+
+func Benchmark_UnmarshalS(b *testing.B) {
+	var user User
+	for i := 0; i < b.N; i++ {
+		func() {
+			UnmarshalS(data2[0], &user)
+			UnmarshalS(data2[1], &user)
+			UnmarshalS(data2[2], &user)
+			UnmarshalS(data2[3], &user)
+			UnmarshalS(data2[4], &user)
+		}()
+	}
 }
--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -1,31 +1,37 @@
+//go:generate easyjson -all stats.go
+
 package hw10programoptimization
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"io"
 	"regexp"
 	"strings"
+
+	"github.com/mailru/easyjson"
 )
 
 type User struct {
-	ID       int
-	Name     string
-	Username string
-	Email    string
-	Phone    string
-	Password string
-	Address  string
+	ID       int    `json:"Id"`
+	Name     string `json:"Name"`
+	Username string `json:"Username"`
+	Email    string `json:"Email"`
+	Phone    string `json:"Phone"`
+	Password string `json:"Password"`
+	Address  string `json:"Address"`
 }
 
 type DomainStat map[string]int
 
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
-	u, err := getUsers(r)
+	var u users
+	err := getUsers(r, &u)
 	if err != nil {
 		return nil, fmt.Errorf("get users error: %w", err)
 	}
-	return countDomains(u, domain)
+	return countDomains(&u, domain)
 }
 
 type users [100_000]User
@@ -34,19 +40,23 @@ func Unmarshal(line string, user *User) error {
 	return json.Unmarshal([]byte(line), &user)
 }
 
-func getUsers(r io.Reader) (result users, err error) {
-	content, err := io.ReadAll(r)
-	if err != nil {
-		return
-	}
+func UnmarshalS(line string, user *User) error {
+	return easyjson.Unmarshal([]byte(line), user)
+}
 
-	lines := strings.Split(string(content), "\n")
-	for i, line := range lines {
+func getUsers(r io.Reader, result *users) (err error) {
+	scanner := bufio.NewScanner(r)
+
+	i := 0
+	for scanner.Scan() {
+		line := scanner.Text()
+
 		var user User
-		if err = Unmarshal(line, &user); err != nil {
+		if err = UnmarshalS(line, &user); err != nil {
 			return
 		}
 		result[i] = user
+		i++
 	}
 	return
 }
@@ -85,7 +95,7 @@ func updateDomainStatS(email string, domainStat *DomainStat) {
 	(*domainStat)[key]++
 }
 
-func countDomains(u users, domain string) (DomainStat, error) {
+func countDomains(u *users, domain string) (DomainStat, error) {
 	result := make(DomainStat)
 
 	for _, user := range u {
