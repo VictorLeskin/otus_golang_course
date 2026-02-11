272cd236f53c015e17223f283f8857efecb46bda
Victor Leskin <victorleskin16@gmail.com>
2026-02-11 23:38:35 +0300

    Finished class of memory storage of events

--- hw12_13_14_15_16_calendar/internal/storage/event.go
+++ hw12_13_14_15_16_calendar/internal/storage/event.go
@@ -1,7 +1,29 @@
 package storage
 
+import (
+	"context"
+	"fmt"
+	"time"
+)
+
 type Event struct {
-	ID    string
-	Title string
-	// TODO
+	ID          string
+	Title       string
+	Description string
+	StartTime   time.Time
+	EndTime     time.Time
+	UserID      string
 }
+
+type Storage interface {
+	CreateEvent(ctx context.Context, event *Event) error
+	UpdateEvent(ctx context.Context, event *Event) error
+	DeleteEvent(ctx context.Context, id string) error
+	GetEvent(ctx context.Context, id string) (*Event, error)
+	ListEvents(ctx context.Context, userID string) ([]*Event, error)
+}
+
+var (
+	ErrEventNotFound = fmt.Errorf("an event not found")
+	ErrEventExists   = fmt.Errorf("this event exists")
+)
