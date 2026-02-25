2358cc100409d30daa05d0c9b5491dd340e20c4a
Victor Leskin <victorleskin16@gmail.com>
2026-02-25 23:36:04 +0300

    add function for HTTP API UpdateEvent

--- hw12_13_14_15_16_calendar/internal/server/http/server.go
+++ hw12_13_14_15_16_calendar/internal/server/http/server.go
@@ -145,13 +145,58 @@ func (s *Server) handleCreateEvent(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(resp)
 }
 
-// handleGetEvent — GET /events/{id} ...
-func (s *Server) handleGetEvent(w http.ResponseWriter, r *http.Request) {
-	// TODO: имплементация
-}
-
 // handleUpdateEvent — PUT /events/{id} ...
 func (s *Server) handleUpdateEvent(w http.ResponseWriter, r *http.Request) {
+	// 1. Читаем JSON из тела.
+	var req UpdateEventRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		s.log.Infof("HTTP Update Error: invalid json %s", err.Error())
+		http.Error(w, "invalid json", http.StatusBadRequest)
+		return
+	}
+
+	s.log.Infof("HTTP Update/Request: title=%q, user_id=%s", req.Title, req.UserID)
+
+	// 2. Преобразуем в storage.Event.
+	event := &storage.Event{
+		Title:       req.Title,
+		Description: req.Description,
+		StartTime:   req.StartTime,
+		EndTime:     req.EndTime,
+		UserID:      req.UserID,
+	}
+
+	// 3. Вызываем ТОТ ЖЕ storage, что и gRPC!.
+	err := s.storage.UpdateEvent(r.Context(), event)
+	if err != nil {
+		// Возвращаем JSON с ошибкой.
+		s.log.Infof("HTTP Update Error: event creating failed %s", err.Error())
+		json.NewEncoder(w).Encode(map[string]string{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	// 4. Преобразуем в response DTO.
+	resp := EventResponse{
+		ID:          event.ID,
+		Title:       event.Title,
+		Description: event.Description,
+		StartTime:   event.StartTime,
+		EndTime:     event.EndTime,
+		UserID:      event.UserID,
+	}
+
+	s.log.Infof("HTTP Update/Response: title=%q, user_id=%s", req.Title, req.UserID)
+
+	// 4. Возвращаем JSON с созданным событием.
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(resp)
+}
+
+// handleGetEvent — GET /events/{id} ...
+func (s *Server) handleGetEvent(w http.ResponseWriter, r *http.Request) {
 	// TODO: имплементация
 }
 
